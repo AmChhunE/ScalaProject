4264dcc5dbae7ca8b34fb2689a77ea80082083ba
Chhuny <chhuny@github.com>
2018-06-24 15:55:19 +0700

    demo

--- forloop.scala
+++ forloop.scala
@@ -1,1 +1,13 @@
-println("hello world");
+object demo
+{
+  def main(args:Array[String])
+  {
+    print("Input the value: ")
+    var num:Int = scala.io.StdIn.readInt();
+    var i:Int = 0;
+    for(i <- (0 to num) if(i%4!=0))
+    {
+      print(i+" ");
+    }
+  }
+}
